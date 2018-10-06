d8840ee2236f5dbaab57330b9e741b8b8de9b826
rishabh <31166087+dthinkcs@users.noreply.github.com>
2018-10-06 16:09:19 +0530

    Create recur2_count_up.py

---
+++ recur/recur2_count_up.py
@@ -0,0 +1,13 @@
+
+def count_up(n):
+    # Step0: how can I break this problem into smaller parts of the same problem
+    # Step1: oh yeah count_up(12) is just first count_up(11) then print(12)
+    # Step2: base case: in which case do I dont need to do anywork; its simple; its trivial
+    if n == 1:
+        print(1)
+    else:
+        count_up(n - 1)
+        print(n)
+        
+count_up(12)
+# Step whatever: verify in your mind by drawing the recursion tree diagram
